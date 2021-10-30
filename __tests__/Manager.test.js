c5473ef0065e2f9120aa4afb63b67d78d1420563
insomniKat <ru3ykat@gmail.com>
2021-10-30 18:32:26 -0400

    manager class and tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,9 +2,9 @@
 const Manager = require('../lib/Manager');
 
 // creating manager object add office number
-test('set officeNumber with constructor function', () => {
+test('set manager officeNumber using constructor function', () => {
     const officeNumber = '131313';
-    const employee = new Manager('Kat', '1', 'ru3ykat@gmail.com', '131313');
+    const employee = new Manager('Suzi Que', '777', 'suzi.que@gmailc.om', '131313');
 
     expect(employee.officeNumber).toBe(officeNumber);
 });
@@ -12,7 +12,9 @@ test('set officeNumber with constructor function', () => {
 // get role from getRole()
 test('gets employee role', () => {
     const role = 'Manager';
-    const employee  = new Manager('Kat', '1', 'ru3ykat@gmail.com', '131313');
+    const employee = new Manager('Suzi Que', '777', 'suzi.que@gmailc.om', '131313');
 
     expect(employee.getRole()).toBe(role);
-})
+});
+
+// 
