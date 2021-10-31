75da47f42a40c168ee3259d99112d5ddc432979b
insomniKat <ru3ykat@gmail.com>
2021-10-30 20:12:42 -0400

    complete classes

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const fs = require('fs');
+const util = require('util');
 const inquirer = require('inquirer');
+const generateHtml = require('./util/generateHtml');
+
+const Manager = require('./lib/Engineer');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const addManager = () =>{
+    return inquirer
+        .prompt ([
+        {
+            type: 'input',
+            
+        }
+    ])
+}
